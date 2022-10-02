40dd7380cfffe1065edf7846ed844d83d909fe1f
korpo <diagenin@gmail.com>
2022-10-02 13:27:59 +0300

    part_3 is done

--- src/part3.sh
+++ src/part3.sh
@@ -1,77 +1,81 @@
 #!/bin/bash
+source ./libs/colors_bash.sh
+PARAMETR_1=${NORMAL}
+PARAMETR_2=${NORMAL}
+PARAMETR_3=${NORMAL}
+PARAMETR_4=${NORMAL}
 
-# Обозначения цветов: (1 - white, 2 - red, 3 - green, 4 - blue, 5 – purple, 6 - black)  
-# **Параметр 1** - это фон названий значений (HOSTNAME, TIMEZONE, USER и т.д.)  
-# **Параметр 2** - это цвет шрифта названий значений (HOSTNAME, TIMEZONE, USER и т.д.)  
-# **Параметр 3** - это фон значений (после знака '=')  
-# **Параметр 4** - это цвет шрифта значений (после знака '=')
-PARAMETR_1='\033[0m'
-PARAMETR_2='\033[0m'
-PARAMETR_3='\033[0m'
-PARAMETR_4='\033[0m'
-DEFAULT_COLOR='\033[0m'
+function error_1_3() {
+echo "1ый и 3ий параметры совпадают, пожалуйста перезапустите скрипт с новыми параметрами"
+exit
+}
 
-function check_color_param_2() {
+function error_2_4() {
+echo "2ый и 4ий параметры совпадают, пожалуйста перезапустите скрипт с новыми параметрами"
+exit
+}
+
+function check_color_param_2_4() {
 if [ "$1" = "1" ]
 then
-echo '\e[1m'
+echo $NORMAL
 fi
 
 if [ "$1" = "2" ]
 then
-echo '\e[31m'
+echo $RED
 fi
 
 if [ "$1" = "3" ]
 then
-echo '\e[32m'
+echo $GREEN
 fi
 
 if [ "$1" = "4" ]
 then
-echo '\e[34m'
+echo $BLUE
 fi
 
 if [ "$1" = "5" ]
 then
-echo '\e[0;35m'
+echo $MAGENTA
 fi
 
 if [ "$1" = "6" ]
 then
-echo '\e[0;30m'
+echo $BLACK
 fi
 }
 
-function check_color_param_1() {
+function check_color_param_1_3() {
 if [ "$1" = "1" ]
 then
-echo '\e[7;1m'
+echo $BGGRAY
 fi
 
 if [ "$1" = "2" ]
 then
-echo '\e[7;31m'
+echo $BGRED
 fi
 
 if [ "$1" = "3" ]
 then
-echo '\e[7;32m'
+echo $BGGREEN
 fi
 
 if [ "$1" = "4" ]
 then
-echo '\e[7;34m'
+echo $BGBLUE
 fi
 
 if [ "$1" = "5" ]
 then
-echo '\e[7;35m'
+echo $BGMAGENTA
 fi
 
 if [ "$1" = "6" ]
 then
-echo '\e[7;30m'
+echo $BGBLACK
 fi
 }
 
@@ -81,84 +85,98 @@ re='[1-6]'
 
 if [[ $1 =~ $re ]]
 then
-PARAMETR_1=$(check_color_param_1 $1)
+
+if [[ $1 = $3 ]]
+then
+error_1_3
+else
+PARAMETR_1=$(check_color_param_1_3 $1)
+fi
+
 fi
 
 if [[ $2 =~ $re ]]
 then
-PARAMETR_2=$(check_color_param_2 $2)
+
+if [[ $2 = $4 ]]
+then
+error_2_4
+else
+PARAMETR_2=$(check_color_param_2_4 $2)
+fi
+
 fi
 
 if [[ $3 =~ $re ]]
 then
-PARAMETR_3=$(check_color_param_2 $3)
+PARAMETR_3=$(check_color_param_1_3 $3)
 fi
 
 if [[ $4 =~ $re ]]
 then
-PARAMETR_4=$(check_color_param_2 $4)
+PARAMETR_4=$(check_color_param_2_4 $4)
 fi
 }
 
 function part_2() {
 #сетевое имя
 HOSTNAME=$(hostnamectl | awk '{print $3;} ' | head -1)
-echo -e "${PARAMETR_1}${PARAMETR_2}HOSTNAME${DEFAULT_COLOR} = $HOSTNAME"
+echo -e "${PARAMETR_1}${PARAMETR_2}HOSTNAME${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$HOSTNAME${NORMAL}"
 
 #временная зона в виде: America/New_York UTC -5
 type_zone=$(timedatectl | awk '{print $3}' | awk '(NR ==4)')
 time_zone=$(timedatectl | awk '{print $6}' | awk '(NR==2)')
 difference_time_zone=$(timedatectl | awk '{print $5}' | awk '(NR==4)' | cut -b -2)
-echo -e "${PARAMETR_1}TIMEZONE${DEFAULT_COLOR} = $type_zone $time_zone $difference_time_zone"
+echo -e "${PARAMETR_1}${PARAMETR_2}TIMEZONE${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$type_zone $time_zone $difference_time_zone${NORMAL}"
 
 #текущий пользователь который запустил скрипт
 USER=$(users)
-echo -e "${PARAMETR_1}USER${DEFAULT_COLOR} = $USER"
+echo -e "${PARAMETR_1}${PARAMETR_2}USER${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$USER${NORMAL}"
 
 #тип и версия операционной системы
 type_OS=$(hostnamectl | awk '{print $3}' | awk '(NR==6)')
 version_OS=$(hostnamectl | awk '{print $3}' | awk '(NR==7)')
-echo -e "${PARAMETR_1}OS${DEFAULT_COLOR} = $type_OS $version_OS"
+echo -e "${PARAMETR_1}${PARAMETR_2}OS${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$type_OS $version_OS${NORMAL}"
 
 #текущее время в виде: 12 May 2020 12:24:36
 DATE=$(date "+%d %B %Y %T")
-echo -e "${PARAMETR_1}DATE${DEFAULT_COLOR} = $DATE"
+echo -e "${PARAMETR_1}${PARAMETR_2}DATE${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$DATE${NORMAL}"
 
 #время работы системы
 UPTIME=$(uptime -p)
-echo -e "${PARAMETR_1}UPTIME${DEFAULT_COLOR} = $UPTIME"
+echo -e "${PARAMETR_1}${PARAMETR_2}UPTIME${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$UPTIME${NORMAL}"
 
 #время работы системы в секундах
 UPTIME_SECOND=$(awk '{print $1}' /proc/uptime)
-echo -e "${PARAMETR_1}UPTIME_SECOND${DEFAULT_COLOR} = $UPTIME_SECOND seconds"
+echo -e "${PARAMETR_1}${PARAMETR_2}UPTIME_SECOND${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$UPTIME_SECOND seconds${NORMAL}"
 
 #ip-адрес машины в любом из сетевых интерфейсов
 IP=$(ip a | awk '{print $2}' | awk '(NR==3)')
-echo -e "${PARAMETR_1}IP${DEFAULT_COLOR} = $IP"
+echo -e "${PARAMETR_1}${PARAMETR_2}IP${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$IP${NORMAL}"
 
 #сетевая маска любого из сетевых интерфейсов в виде: xxx.xxx.xxx.xxx
 MASK=$(ip a | awk '{print $2}' | awk '(NR==2)')
-echo -e "${PARAMETR_1}MASK${DEFAULT_COLOR} = $MASK"
+echo -e "${PARAMETR_1}${PARAMETR_2}MASK${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$MASK${NORMAL}"
 
 #ip шлюза по умолчанию
 GATEWAY=$(ip route | awk '{print $3}' | awk '(NR==1)')
-echo -e "${PARAMETR_1}GATEWAY${DEFAULT_COLOR} = $GATEWAY"
+echo -e "${PARAMETR_1}${PARAMETR_2}GATEWAY${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$GATEWAY${NORMAL}"
 
 #размер оперативной памяти в Гб c точностью три знака после запятой в виде: 3.125 GB
 RAM_TOTAL=$(awk '{print $2}' /proc/meminfo | awk '(NR==1)' | awk '{printf "%.3f",$1/1024/1024}')
-echo -e "${PARAMETR_1}RAM_TOTAL${DEFAULT_COLOR} = $RAM_TOTAL GB"
+echo -e "${PARAMETR_1}${PARAMETR_2}RAM_TOTAL${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$RAM_TOTAL GB${NORMAL}"
 RAM_USED=$(free | awk '{print $3}' | awk '(NR==2)' | awk '{printf "%.3f", $1/1024/1024}')
-echo -e "${PARAMETR_1}RAM_USED${DEFAULT_COLOR} = $RAM_USED GB"
+echo -e "${PARAMETR_1}${PARAMETR_2}RAM_USED${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$RAM_USED GB${NORMAL}"
 RAM_FREE=$(free | awk '{print $4}' | awk '(NR==2)' | awk '{printf "%.3f", $1/1024/1024}')
-echo -e "${PARAMETR_1}RAM_FREE${DEFAULT_COLOR}  = $RAM_FREE GB"
+echo -e "${PARAMETR_1}${PARAMETR_2}RAM_FREE${NORMAL}  = ${PARAMETR_3}${PARAMETR_4}$RAM_FREE GB${NORMAL}"
 
 # размер рутового раздела в Mб с точностью два знака после запятой в виде: 254.25 MB
 SPACE_ROOT=$(df -i / | awk '{print $2}' | awk '(NR==2)' | awk '{printf "%.2f", $1/1024}')
 SPACE_ROOT_FREE=$(df -i / | awk '{print $4}' | awk '(NR==2)' | awk '{printf "%.2f", $1/1024}')
 SPACE_ROOT_USED=$(df -i / | awk '{print $3}' | awk '(NR==2)' | awk '{printf "%.2f", $1/1024}')
-echo -e "${PARAMETR_1}SPACE_ROOT${DEFAULT_COLOR} = $SPACE_ROOT MB"
-echo -e "${PARAMETR_1}SPACE_ROOT_USED${DEFAULT_COLOR} = $SPACE_ROOT_USED MB"
-echo -e "${PARAMETR_1}SPACE_ROOT_FREE${DEFAULT_COLOR} = $SPACE_ROOT_FREE MB"
+echo -e "${PARAMETR_1}${PARAMETR_2}SPACE_ROOT${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$SPACE_ROOT MB${NORMAL}"
+echo -e "${PARAMETR_1}${PARAMETR_2}SPACE_ROOT_USED${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$SPACE_ROOT_USED MB${NORMAL}"
+echo -e "${PARAMETR_1}${PARAMETR_2}SPACE_ROOT_FREE${NORMAL} = ${PARAMETR_3}${PARAMETR_4}$SPACE_ROOT_FREE MB${NORMAL}"
 }
 
 function main() {
